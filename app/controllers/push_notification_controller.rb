d2ea2a65ec4b3bb66d0697c316dc51a4b6beea17
Rafael Nunes Vieira <rafael.vieira@ima.sp.gov.br>
2017-07-04 10:08:17 -0300

    adicionando método para enviar mensagens

--- app/controllers/push_notification_controller.rb
+++ app/controllers/push_notification_controller.rb
@@ -1,4 +1,8 @@
+require 'fcm'
+
 class PushNotificationController < ActionController::API
+
+  FCMServerKey = 'xxxxxxxxxxxxxxxxxxxx'.freeze
 
   def register_device
     device = Device.find_or_create_by register: params[:register]
@@ -6,6 +10,34 @@ class PushNotificationController < ActionController::API
     render json: {
       message: 'Registro efetuado com sucesso!',
       device: device
+    }
+  end
+
+  def send_message
+    fcm = FCM.new(FCMServerKey, proxy_options)
+
+    registration_ids = Device.pluck(:register)
+
+    payload = { message: params[:message] }
+
+    options = {
+      data: payload,
+      collapse_key: 'updated_score'
+    }
+
+    fcm.send(registration_ids, options)
+
+    render plain: "Mensagens enviadas para [#{registration_ids.join(', ')}]"
+  end
+
+  private
+
+  # Caso seu serviço esteja rodando por de trás de um proxy
+  # Você pode configurar-lo neste método
+  def proxy_options
+    {
+      # http_proxyaddr: 'xxxx.xxx.xxx.xx',
+      # http_proxyport: 'xxxx'
     }
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   post 'register/device', to: 'push_notification#register_device'
+  post 'send/message', to: 'push_notification#send_message'
 end
